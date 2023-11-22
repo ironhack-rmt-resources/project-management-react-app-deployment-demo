6d00ea13dbd12a48dbeabb3ae9169d92b5c547b1
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2023-11-22 08:54:36 +0100

    force runtime error II

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,21 @@ import ProjectListPage from "./pages/ProjectListPage";
 import CreateProjectPage from "./pages/CreateProjectPage";
 import ProjectDetailsPage from "./pages/ProjectDetailsPage";
 import EditProjectPage from "./pages/EditProjectPage";
+import { useState } from 'react';
 
 function App() {
 
-  const amount = 42;
-  amount = 30;
+  const [movies, setMovies] = useState();
 
   return (
     <div className="App">
       
       <Navbar />
+
+      { movies.map( movie => {
+        return <p>this is a movie</p>
+      }) }
+
 
       <Routes>
         <Route path="/" element={ <HomePage /> } />
